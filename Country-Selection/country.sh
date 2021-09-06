a9716f79eaf46a20cee790f5fab1d7aa622f5968
bluelab58 <bluelab58@gmail.com>
2021-09-06 15:13:45 -0400

    3rd commit

--- Country-Selection/country.sh
+++ Country-Selection/country.sh
@@ -8,5 +8,3 @@ import random
 Cities = ["WASHINGTON DC", "NEW YORK", "LONDON", "LAGOS", "PARIS", "BERLIN", "ACCRA", "PRETORIA", "ABUJA", "TOKYO"]
 random_countries = random.choice(Countries)
 print ("Selected city is : " + str(random_countries)) > /var/www/html/index.html
-
-to man
