86193b2a2eecda66f7e920ff5d606077f5c1ffc1
bluelab58 <bluelab58@gmail.com>
2021-09-08 08:56:04 -0400

    commit

--- Country-Selection/country.sh
+++ Country-Selection/country.sh
@@ -6,5 +6,5 @@ systemctl start httpd.service
 sytemctl enable httpd.service
 import random
 Cities = ["WASHINGTON DC", "NEW YORK", "LONDON", "LAGOS", "PARIS", "BERLIN", "ACCRA", "PRETORIA", "ABUJA", "TOKYO"]
-random_countries = random.choice(Countries)
+random_countries = random.choice(Cities)
 print ("Selected city is : " + str(random_countries)) > /var/www/html/index.html
